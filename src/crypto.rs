c0ce93e4bb14979da616162977fabb880ee70a3b
flamen <jitang.zheng@gmail.com>
2015-10-05 13:10:04 -0700

    hmac md5

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,12 +5,20 @@ extern crate rustc_serialize;
 use std::cmp::PartialEq;
 use std::borrow::BorrowMut;
 use std::iter::Extend;
-use self::crypto::{sha1, sha2, md5, digest};
+use self::crypto::{sha1, sha2, md5, digest, hmac};
+use self::crypto::mac::{Mac};
 use self::rand::Rng;
 use self::rustc_serialize::base64::{ToBase64, FromBase64, STANDARD};
+use self::rustc_serialize::hex::ToHex;
 
 pub trait Encryptor {
-    
+
+    fn en_aes(&self, k: &str) -> String;
+    fn de_aes(&self, k: &str) -> String;
+
+    fn sum_hmac_md5(&self, k: &str) -> String;
+    fn chk_hmac_md5(&self, s: &str, k: &str) -> bool;
+          
     fn sum_sha1(&self) -> String;
     fn chk_sha1(&self, s: &str) -> bool;
     fn sum_sha256(&self) -> String;
@@ -28,10 +36,34 @@ pub trait Encryptor {
     fn sum<T: digest::Digest>(&self, dig: &mut T) -> String;
     fn ssum<T: digest::Digest>(&self, dig: &mut T, l: usize) -> String;
     fn schk<T: digest::Digest>(&self, dig: &mut T, s: &str, l: usize) -> bool;
+
+    fn sum_hmac<T: digest::Digest>(&self, dig:  T, k: &str) -> String;
   
 }
 
 impl Encryptor for String {
+
+    fn en_aes(&self, k: &str) -> String {
+        k.to_string()
+    }
+    fn de_aes(&self, k: &str) -> String {
+        k.to_string()
+    }
+    
+    fn sum_hmac_md5(&self,  k: &str) -> String {
+        self.sum_hmac(md5::Md5::new(), k)            
+    }
+    fn chk_hmac_md5(&self, s:&str,  k: &str) -> bool {
+        let d = s.to_string().sum_hmac_md5(k);
+        self.eq(&d)        
+    }
+    
+    fn sum_hmac<T: digest::Digest>(&self, dig:  T, k: &str) -> String {
+        let mut hm = hmac::Hmac::new(dig, k.as_bytes());
+        hm.input(&self.as_bytes());
+        hm.result().code().to_hex()
+    }
+
         
     fn sum_sha1(&self) -> String {        
         let mut dig = sha1::Sha1::new();        
@@ -128,6 +160,7 @@ fn test_encryptor() {
     let hello = "hello".to_string();
     let world = "world";
     let size = 6;
+    let key = "123456";
     
     let s1 = hello.sum_sha1();
     assert!(s1.chk_sha1(&hello));
@@ -155,9 +188,14 @@ fn test_encryptor() {
     assert!(!ss256.chk_ssha256(world, size));
     println!("run: doveadm pw -t {}{} -p {}", "{SSHA256}", ss256, hello);
     
-    let ss512 = hello.sum_ssha512(size);
-    assert!(!ss512.is_empty());
+    let ss512 = hello.sum_ssha512(size);    
     assert!(ss512.chk_ssha512(&hello, size));
     assert!(!ss512.chk_ssha512(world, size));
     println!("run: doveadm pw -t {}{} -p {}", "{SSHA512}", ss512, hello);
+
+    let hmd5 = hello.sum_hmac_md5(key);    
+    assert!(hmd5.chk_hmac_md5(&hello, key));
+    assert!(!hmd5.chk_hmac_md5(world, key));
+    println!("hmac md5: {}", hmd5);
+    
 }
